8d60f1b094820648f056b13a0eba71ab5206ba68
Alex <alexgmark@gmail.com>
2026-03-05 17:18:28 +0000

    New branch

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import injectHTML from 'vite-plugin-html-inject';
+
+export default defineConfig({
+	plugins: [tailwindcss(), injectHTML()],
+	build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        components: 'components.html',
+        collections: 'collections.html',
+        products: 'products.html',
+        dashboard: 'dashboard.html'
+      },
+    },
+  },
+});
